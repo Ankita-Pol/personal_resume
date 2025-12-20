3fec38e709f01444240e57450b57b3d4635212e9
Ankita-Pol <theankita473@gmail.com>
2025-12-20 18:45:09 +0530

    rendered personal summary

--- src/app/layout/main-content/main-content.ts
+++ src/app/layout/main-content/main-content.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './main-content.html',
-  styleUrl: './main-content.scss',
+  styleUrl: './main-content.scss'
 })
 export class MainContent {
+  @Input() personal: any;
+
+  @Input() education: any;
+  @Input() projects: any;
+  @Input() experience: any;
+  @Input() achievements: any;
+  @Input() extracurricular: any;
+  @Input() references: any;
 
 }
