bdd4331ccb7799a7d7f93d4f4c9d88bffb6680a5
Ankita-Pol <theankita473@gmail.com>
2025-12-20 16:25:12 +0530

    load resume data in root apps component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ResumeService } from './core/services/resume.service';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { MainContent } from './layout/main-content/main-content';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, Sidebar, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('resume_ui');
+export class AppComponent implements OnInit {
+
+  resumeData: any;
+
+  constructor(private resumeService: ResumeService) {}
+
+  ngOnInit(): void {
+    this.resumeService.getResumeData().subscribe((data) => {
+      this.resumeData = data;
+    });
+  }
 }
