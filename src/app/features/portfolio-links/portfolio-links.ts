3f1c57677efac5f393d3d90170f179fa397c29d8
Ankita-Pol <theankita473@gmail.com>
2025-12-20 15:41:52 +0530

    add resume project structure and assets

---
+++ src/app/features/portfolio-links/portfolio-links.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-links',
+  imports: [],
+  templateUrl: './portfolio-links.html',
+  styleUrl: './portfolio-links.scss',
+})
+export class PortfolioLinks {
+
+}
